261337068c58f17de904a6eec115b9c6e86a698a
Natthawat Pomsuwan <natthawat.pm@rmuti.ac.th>
2025-04-26 18:06:34 +0700

    forDeploy

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -90,7 +90,7 @@ class UserService {
   }
 
   static async getUserByEmail(email) {
-    return await prisma.User.findUnique({
+    return await prisma.user.findUnique({
       where: { email },
       include: {
         personnelType: true,
