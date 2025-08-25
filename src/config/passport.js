45cb8424b8a8fb6d1b37d8c03f943867791c3b68
Natthawat Pomsuwan <natthawat.pm@rmuti.ac.th>
2025-08-25 17:48:23 +0700

    test oauth 4

--- src/config/passport.js
+++ src/config/passport.js
@@ -100,7 +100,7 @@ passport.use(
   new GoogleStrategy({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback"
+      callbackURL: "https://backend-faculty-of-engineering-leave.onrender.com/auth/google/callback"
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
