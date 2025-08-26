db865e82cdc8e39e3122791499efab644e7f5954
Natthawat Pomsuwan <natthawat.pm@rmuti.ac.th>
2025-08-26 18:36:40 +0700

    test oauth 19

--- src/routes/auth-route.js
+++ src/routes/auth-route.js
@@ -105,7 +105,7 @@ router.get(
   (req, res) => res.json(req.user)
 );
 
-router.get("/profile", authenticate, async (req, res) => {
+router.get("/profile", authenticateJWT, async (req, res) => {
   // req.user มีข้อมูล user ที่ login แล้ว
   res.json({ message: "This is protected", user: req.user });
 });
