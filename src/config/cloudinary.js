a3feb59d79227bada1491b24f7f558f341b57d17
LeChevalier <assawinknw48@gmail.com>
2025-12-25 19:17:57 +0700

    add env=test cloudinary

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,13 +1,21 @@
 const cloudinary = require('cloudinary').v2;
 
+const isTestEnv =
+  process.env.NODE_ENV === "test" ||
+  process.env.JEST_WORKER_ID !== undefined;
+
 if (!process.env.CLOUDINARY_SECRET) {
+  if (isTestEnv) {
+    process.env.CLOUDINARY_SECRET = "test";
+  } else {
     throw new Error("CLOUDINARY_SECRET environment variable is missing!");
+  }
 }
 
 cloudinary.config({
-    cloud_name: 'dasrmjl0f',
-    api_key: '316341844843451',
-    api_secret: process.env.CLOUDINARY_SECRET,
+  cloud_name: 'dasrmjl0f',
+  api_key: '316341844843451',
+  api_secret: process.env.CLOUDINARY_SECRET,
 });
 
 module.exports = cloudinary;
